d9c898e7edfa32aede5cef66aeaf417b59e0dd21
Jenny-vaz <91665666+Jenny-vaz@users.noreply.github.com>
2021-10-28 15:55:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,23 +5,25 @@
     image_format : 'png',
     png_quality:90
   });
+
   camera = document.getElementById("camera");
   
-  Webcam.attach( "# camera" ); 
+  Webcam.attach( "#camera" ); 
+
 
    function take_snapshort()
    {
        Webcam.snap(function(data_url){
-           document.getElementById("result").innerHTML = '<imgid"captured_image" src="'+ data_url+'"/>';
+           document.getElementById("result").innerHTML = '<img id"captured_image" src="'+ data_url+'"/>';
        });
    }
 
-   console.log('ml5 version:', ml5.version);
+        console.log('ml5 version:', ml5.version);
 
-   classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
-                 
-   function modelLoaded(){
-       console.log('model Loaded')
+        classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+   
+    function modelLoaded(){
+       console.log('model Loaded!');
    }
 
    function check()
@@ -33,6 +35,8 @@
    function gotResult(error, result){
        if(errow){
            console.error(error)
+       }else{
+        console.error(result)
            document.getElementById("result_object_name").innerHTML = results[0].label;
            document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
        }
